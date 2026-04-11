c5f3276c7546bae6616cb80d2e9a562652897af3
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-11 14:39:38 +0600

    create a card component

--- React-Restart/src/App.jsx
+++ React-Restart/src/App.jsx
@@ -1,55 +1,13 @@
-import { useState, useEffect } from "react";
+import Card from './Card'
+function App(){
+  return(
+    <>
+    <Card />
+    </>
+    
 
-function App() {
-  //conditional rendaring
-  
-  const [counterVisible, setCounterVisible] = useState(false);
-
-  useEffect(function () {
-    setInterval(function () {
-      setCounterVisible(
-        c => !c
-      )
-    }, 5000)
-  },[] )
-  return <div>
-    {counterVisible ? <Counter></Counter> : null}
-  </div>
+  )
 
 }
 
-
-
-
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  //clearInterval
-  useEffect(function () {
-    console.log("hi there")
-   let clock = setInterval(function () {
-      console.log("From inside the interval");
-      setCount(count + 1)
-    }, 1000)
-    return function (){
-      console.log("unmount")
-      clearInterval(clock);
-    }
-     
-  }, [])
-
- 
-
-  console.log("render");
-
-  return <div>
-    <h1 id="text">{count}</h1>
-    <button>Increase Count</button>
-  </div>
-
-
-
-}
-
-export default App
+export default App;
---
+++ React-Restart/src/Card.jsx
@@ -0,0 +1,13 @@
+import image from './assets/cat.jpg'
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src={image } className="card-image" alt="profile picture" />
+            <h2 className='card-tile'>Mehedi Hasan</h2>
+            <p>I am a programmer who is learning react and i like to play video game</p>
+
+        </div>
+    )
+}
+
+export default Card;
