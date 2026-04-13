c0ba92edd019bdb6754bb9d59028d447352e85ec
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-13 22:47:43 +0600

    use of routing in react

--- React-Restart/src/App.jsx
+++ React-Restart/src/App.jsx
@@ -1,28 +1,59 @@
-function App(){
+import { Fragment } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+function App() {
+  //SPAs (signle page application)
   return (
-    <div style={{display:"flex"}}>
-      <Card>
-        <div>
-        What do you want to post?
-        <br />
-        <input type="text" />
-         </div>
-      </Card>
+    <>
+      <BrowserRouter>
       
+        <Link to={"/"}>Allen</Link>
+        |
+        <Link to={"/neet/online-coaching-class-11"}>Class 11</Link>
+        |
+        <Link to={"/neet/online-coaching-class-12"}>Class 12</Link>
 
-    </div>
-
+        <Routes>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 
+
+};
+
+
+function Class11Program() {
+  return (
+    <div>
+      NEET programs for Class 11th
+    </div>
+  )
 }
 
-function Card({children}){
-  return <div style={{background:"gray", borderRadius:10 , color:"black", padding: 10, margin: 10}}>
-    {children}
-  </div>
-
+function Class12Program() {
+  return (
+    <div>
+      NEET programs for Class 12th
+    </div>
+  )
 }
+
+function Landing() {
+  return (
+    <div>
+      Wellcome to the Alen website
+    </div>
+  )
+}
+
+
+
+
+
 
 
 
