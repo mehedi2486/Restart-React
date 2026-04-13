dca3d0360f661d4f4f386f3f6e31eb970e8d5d3b
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-13 13:26:05 +0600

    use the children props

--- React-Restart/src/App.jsx
+++ React-Restart/src/App.jsx
@@ -1,52 +1,28 @@
-import { useEffect, useState } from "react";
-//Build a React component that behaves like an auto-incrementing counter with controls.
-function App() {
-  const [count, setCount] = useState(0);
-  const [isRunnign, setIsRunning] = useState(false);
-
-  
- useEffect ( ()=> {
-  let interval;
-  if(isRunnign){
-    interval = setInterval(() => {
-      setCount(c => c + 1)
-    },1000)
-  }
-  return () =>{
-    clearInterval(interval)
-  }
-
- },[isRunnign])
-  
-
-  function start(){
-    setIsRunning(true)
-  }
-  function stop(){
-    setIsRunning(false)
-  }
-
-  function reset(){
-    setIsRunning(false);
-    setCount(0);
-  }
-
-
-
-
+function App(){
 
   return (
-    <div>
-      <button onClick = {start}>Start</button>
-      <button onClick = {stop}>Stop</button>
-      <button onClick={reset}>Reset</button>
-      <h1>{count}</h1>
-      <p>Status: {isRunnign ? "Running" : "Stopped"}</p>
+    <div style={{display:"flex"}}>
+      <Card>
+        <div>
+        What do you want to post?
+        <br />
+        <input type="text" />
+         </div>
+      </Card>
+      
+
     </div>
+
   )
 
 }
 
+function Card({children}){
+  return <div style={{background:"gray", borderRadius:10 , color:"black", padding: 10, margin: 10}}>
+    {children}
+  </div>
+
+}
 
 
 
