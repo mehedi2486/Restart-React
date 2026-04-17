2f2b57cc625850400a4ab73f015228f37f7d32d9
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-18 03:15:00 +0600

    state managment

--- React-Restart/src/App.jsx
+++ React-Restart/src/App.jsx
@@ -1,105 +1,41 @@
-//searchable product data table
+import { useState } from "react"
 
-
-function ProductCategoryRow({ category }) {
-    return (
-        <tr>
-            <td colSpan="2">
-                {category}
-            </td>
-        </tr>
-    )
-
+function App(){
+  return(
+    <div>
+      <LightBulb />
+    </div>
+  )
 }
 
-function ProductRow({ product }) {
-    const name = product.stocked ? product.name :
-        <span style={{ color: "red" }}>
-            {product.name}
-        </span>
-
-    return (
-        <tr>
-            <td>{name}</td>
-            <td>{product.price}</td>
-        </tr>
-    )
-
-
+function LightBulb(){
+  const [BulbOn, setBulbOn] = useState(false);
+  return(
+    <div>
+      <BulbState bulbOn = {BulbOn}/>
+      <ToggleBulbState bulbOn = {BulbOn} setBulbOn = {setBulbOn}/>
+    </div>
+  )
 }
 
-function ProductTable({ products }) {
-    const rows = [];
-    let lastCategory = null;
+function BulbState({bulbOn}){
+  return (
+    <div>
+      {bulbOn ? "Bulb on" : "Bulb off"}
 
-    products.forEach((product) => {
-        if (product.category !== lastCategory) {
-            rows.push(
-                <ProductCategoryRow
-                    category={product.category}
-                    key={product.category} />
-            );
-        }
-        rows.push(
-            <ProductRow
-                product={product}
-                key={product.name} />
-        );
-        lastCategory = product.category;
-    })
-
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-        </table>
-    )
-
-
+    </div>
+  )
 }
 
-
-function SearchBar() {
-    return (
-        <form>
-            <input type="text" placeholder="Search..." />
-            <label >
-                <input type="checkbox" />
-                {' '}
-                only show products in stock
-            </label>
-        </form>
-
-    )
-}
-
-function FilterableProductTable({ products }) {
-    return (
-        <div>
-            <SearchBar />
-            <ProductTable products={products} />
-        </div>
-    )
-}
-
-const PRODUCTS = [
-    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-];
-
-function App() {
-    return <FilterableProductTable products={PRODUCTS} />;
+function ToggleBulbState({setBulbOn, bulbOn}){
+  function toggle(){
+    setBulbOn(!bulbOn)
+  }
+   return (
+    <div>
+      <button onClick={toggle}>Toggle the bulb</button>
+    </div>
+   )
 }
 
 export default App;
-
-
