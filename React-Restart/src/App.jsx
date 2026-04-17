7e312f00c077383960dd2fbc554c6a75345b5ed3
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-18 01:37:16 +0600

    static version is complete

--- React-Restart/src/App.jsx
+++ React-Restart/src/App.jsx
@@ -1,8 +1,8 @@
 //searchable product data table
 
 
-function ProductCategoryRow({category}){
-    return(
+function ProductCategoryRow({ category }) {
+    return (
         <tr>
             <td colSpan="2">
                 {category}
@@ -12,11 +12,11 @@ function ProductCategoryRow({category}){
 
 }
 
-function ProductRow({product}){
+function ProductRow({ product }) {
     const name = product.stocked ? product.name :
-    <span style={{color:"red"}}>
-        {product.name}
-    </span>
+        <span style={{ color: "red" }}>
+            {product.name}
+        </span>
 
     return (
         <tr>
@@ -28,6 +28,78 @@ function ProductRow({product}){
 
 }
 
+function ProductTable({ products }) {
+    const rows = [];
+    let lastCategory = null;
 
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category} />
+            );
+        }
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name} />
+        );
+        lastCategory = product.category;
+    })
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    )
+
+
+}
+
+
+function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search..." />
+            <label >
+                <input type="checkbox" />
+                {' '}
+                only show products in stock
+            </label>
+        </form>
+
+    )
+}
+
+function FilterableProductTable({ products }) {
+    return (
+        <div>
+            <SearchBar />
+            <ProductTable products={products} />
+        </div>
+    )
+}
+
+const PRODUCTS = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+
+function App() {
+    return <FilterableProductTable products={PRODUCTS} />;
+}
+
+export default App;
 
 
